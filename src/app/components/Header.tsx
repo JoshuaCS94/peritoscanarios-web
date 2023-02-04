17a01b4fafc5727ea5476b7288a5a9e953f3a06d
Josué Cortina <joshuacs.main@gmail.com>
2023-02-04 21:11:43 +0000

    feat: Make page responsive

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -6,17 +6,17 @@ const EMAIL = 'info@tuperitomedico.com'
 
 export const Header = () => {
   return (
-    <div className='flex w-full items-center justify-between py-4 px-16'>
+    <div className='flex w-full flex-col items-center justify-between py-4 px-16 md:flex-row'>
       <Image src='/logo-full.jpeg' alt='main-image' width={256} height={80} />
-      <p className='flex items-center'>
+      <div className='mt-4 flex flex-col items-center md:mt-0 md:flex-row'>
         <a href={`https://wa.me/${PHONE}`} className='inline-flex items-center gap-2'>
           <IconBrandWhatsapp width={20} height={20} /> {PHONE}
         </a>
-        <span className='mx-2 h-6 w-0.5 bg-gray-900' />
+        <span className='mx-2 hidden h-6 w-0.5 bg-gray-900 md:block' />
         <a href={`mailto:${EMAIL}`} className='inline-flex items-center gap-2 underline'>
           <IconMail width={20} height={20} className='-mb-0.5' /> {EMAIL}
         </a>
-      </p>
+      </div>
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,15 +8,15 @@ const HomePage = () => {
     <main className='h-screen'>
       <Header />
       <div className='pb-8'>
-        <Image src='/background.jpg' alt='background' width={900} height={400} className='float-right ml-16' />
-        <div className='pl-16 pt-40 text-center'>
-          <h1 className='text-5xl font-semibold leading-snug text-blue-900'>
+        <Image src='/background.jpg' alt='background' width={900} height={400} className='md:float-right md:ml-16' />
+        <div className='mt-4 text-center md:mt-0 md:pl-16 md:pt-40'>
+          <h1 className='text-4xl font-semibold leading-snug text-blue-900 md:text-5xl'>
             Valoración de Daño Corporal y Peritaje Médico
           </h1>
-          <button className='mt-12 rounded bg-blue-900 px-4 py-2 text-white'>Reservar ahora</button>
+          <button className='mt-4 rounded bg-blue-900 px-4 py-2 text-white md:mt-12'>Reservar ahora</button>
         </div>
       </div>
-      <div className='clear-both grid grid-cols-2 gap-16 bg-sky-100 p-20'>
+      <div className='clear-both flex flex-col-reverse gap-8 bg-sky-100 p-10 md:grid md:grid-cols-2 md:gap-16 md:p-20'>
         <iframe
           src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d438.5986252605379!2d-16.491143841773543!3d28.42562290000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xc41d51ef908bedd%3A0xc90d780a86de75b1!2sCarr.%20Espa%C3%B1a%2C%2011%2C%2038390%20Sta%20%C3%9Arsula%2C%20Santa%20Cruz%20de%20Tenerife!5e0!3m2!1sen!2ses!4v1675196543857!5m2!1sen!2ses'
           width={600}
@@ -27,7 +27,7 @@ const HomePage = () => {
         ></iframe>
         <div className='flex flex-col justify-center gap-2'>
           <h2 className='text-3xl font-semibold'>Tu Perito Médico</h2>
-          <div className='my-4 h-0.5 w-60 bg-gray-900' />
+          <div className='my-4 h-0.5 w-full bg-gray-900 md:w-60' />
           <p className='text-justify'>
             Gabinete médico dirigido por la <strong>Dra. Masiel Navarro Pérez</strong> (Col. 383807844), Licenciada en
             Medicina en la Universidad de Ciencias Médicas de la Habana, Máster en Valoración de Daño Corporal y
@@ -45,10 +45,10 @@ const HomePage = () => {
           </ul>
         </div>
       </div>
-      <div className='grid grid-cols-2 gap-16 p-20'>
+      <div className='flex flex-col gap-8 p-10 md:grid md:grid-cols-2 md:gap-16 md:p-20'>
         <div>
           <h2 className='text-3xl'>Servicios</h2>
-          <div className='my-4 h-0.5 w-60 bg-gray-900' />
+          <div className='my-4 h-0.5 w-full bg-gray-900 md:w-60' />
           <ul>
             <li>Consulta médica</li>
             <li>Elaboración de informes de aptitud</li>
@@ -64,7 +64,7 @@ const HomePage = () => {
         </div>
         <div>
           <h2 className='text-3xl'>Honorarios</h2>
-          <div className='my-4 h-0.5 w-60 bg-gray-900' />
+          <div className='my-4 h-0.5 w-full bg-gray-900 md:w-60' />
           <p>
             Precios personalizados para cada caso teniendo en cuenta la dificultad del mismo, tiempo empleado, urgencia,
             especialización exigida, utilización de material específico, desplazamientos y asistencia a juicio
