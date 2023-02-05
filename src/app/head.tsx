3cbce706f152bc9e899aa80a2a048ed2ecb238d9
Josué Cortina <joshuacs.main@gmail.com>
2023-02-05 18:28:22 +0000

    feat: Set a proper description

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -3,7 +3,7 @@ export default function Head() {
     <>
       <title>Tu perito médico</title>
       <meta content='width=device-width, initial-scale=1' name='viewport' />
-      <meta name='description' content='TODO: La descripción :)' />
+      <meta name='description' content='Valoración de daño corporal y peritaje médico' />
       <link rel='icon' href='/favicon.png' />
     </>
   )
